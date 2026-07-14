763060a2424a05a8cbeb014891be596c6372e218
erocksom47@gmail.com <eroksom47@gmail.com>
2026-07-14 10:13:16 -0600

    cambios para subir al vps

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Vite config — https://vitejs.dev/config/
-// NOTA: cuando llegues a la FASE 4 (despliegue en el VPS), cambia el valor
-// de "base" para que coincida con la ruta de tu equipo, por ejemplo:
-// base: '/t3_act8_eqXX/'
 export default defineConfig({
-  base: '/',
+  base: '/t3_act8_eq15/',
   plugins: [react(), tailwindcss()],
 })
